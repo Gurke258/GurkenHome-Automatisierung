6c4319a5f70f3cb42f2907c6c0217942fcf28d7e
Steven <36326543+Gurke258@users.noreply.github.com>
2022-04-24 19:25:41 +0200

    Add files via upload

---
+++ Raspberry/ShellHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Raspberry
+{
+    public static class ShellHelper
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
---
+++ Raspberry/Variablen.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Raspberry
+{
+    class Variablen
+    {
+        public class DHT
+        {
+           public static string dhtresult;
+           public static string dhtcommand = "sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 11 22";
+        }
+
+        public class Wäsche
+        {
+            public class Unterwäsche
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Schwarze_Wäsche
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Bunte_Wäsche
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Weiße_Wäsche
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Windeln
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Sonstige
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+                public static int zeit;
+            }
+        }
+
+        public class Pflanzen
+        {
+            public class Erdbeeren
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Tomaten
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+            public class Radieschen
+            {
+                public static Timer timer = new Timer();
+                public static string countDownString;
+                public static DateTime endtime;
+            }
+
+        }
+    }
+}
